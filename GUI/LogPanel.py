dfec72d708d5e56bcbe87ba73fd07113165a2b38
spensicus <52704940+spensicus@users.noreply.github.com>
2020-11-07 11:28:57 -0700

    Basic GUI finished

--- GUI/LogPanel.py
+++ GUI/LogPanel.py
@@ -8,5 +8,10 @@ class LogPanel(wx.Panel):
 
     def __init__(self, parent):
         super().__init__(parent=parent)
-        self.layout = wx.BoxSizer(wx.HORIZONTAL)
-        self.textBox = wx.TextCtrl(self, style=wx.TE_MULTILINE | wx.TE_READONLY)
+        layout = wx.BoxSizer(wx.HORIZONTAL)
+        self.log = wx.TextCtrl(self, style=wx.TE_MULTILINE | wx.TE_READONLY)
+        layout.Add(self.log, 1, wx.EXPAND, 0)
+        self.SetSizer(layout)
+
+    def update(self, actions_text):
+        self.log.AppendText(actions_text)
--- GUI/MapPanel.py
+++ GUI/MapPanel.py
@@ -8,6 +8,10 @@ class MapPanel(wx.Panel):
 
     def __init__(self, parent):
         super().__init__(parent=parent)
-        self.layout = wx.BoxSizer(wx.HORIZONTAL)
+        layout = wx.BoxSizer(wx.HORIZONTAL)
         self.textBox = wx.TextCtrl(self, style=wx.TE_MULTILINE | wx.TE_READONLY)
+        layout.Add(self.textBox, 1, wx.EXPAND, 0)
+        self.SetSizer(layout)
 
+    def update(self, map_string):
+        self.textBox.SetValue(map_string)
