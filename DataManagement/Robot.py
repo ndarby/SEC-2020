1d14931d3dae7a78f7bd31095a0b710c42edbeb8
Nathan Darby <nathan.darby@canadianuavs.ca>
2020-11-07 12:03:20 -0700

    Robot fixed bugs

--- DataManagement/Robot.py
+++ DataManagement/Robot.py
@@ -2,24 +2,34 @@
 
 class Robot(PassTime, AccientalWaitTime, HandoverTime):
     def __init__(self, PassTime, AccientalWaitTime, HandoverTime, RobotID):
-        self.busy = false
-        self.orderDelivered = false
+        self.busy = False
+        self.orderDelivered = True
         self.timeAvailable = 0
         self.passTime = PassTime
         self.accidentalWaitTime = AccientalWaitTime
         self.handoverTime = HandoverTime
         self.currentPath = []
+        self.pathInfo = []
         self.pathIndex = 1
         self.batteryLevel = 100
-        self.currentOrderInfo = []
         self.currentPosition = (0, 0)
         self.distanceTravelled = 0
         self.log = "Robot " + RobotID
 
+    def ReadyForOrder(self):
+        return not self.busy and self.orderDelivered
+
+    def SetOrder(self, orderPath, orderPathInfo):
+        self.currentPath = orderPath
+        self.pathInfo = orderPathInfo
+        self.pathIndex = 0
+        self.orderDelivered = False
+        self.busy = True
+
 
     def Update(self, time):
-        if self.timeAvailable >= time or not self.busy:
-            if self.AttemptMove():
+        if self.timeAvailable >= time or self.busy:
+            if self.AttemptMove(time):
                 self.Move(time)
         else:
             pass
@@ -30,7 +40,7 @@ class Robot(PassTime, AccientalWaitTime, HandoverTime):
         if not self.orderDelivered:
             self.pathIndex += 1
         else:
-            self.pathIndex -+ 1
+            self.pathIndex -= 1
 
         self.timeAvailable = time + self.passTime
         self.distanceTravelled +=1
@@ -38,15 +48,26 @@ class Robot(PassTime, AccientalWaitTime, HandoverTime):
 
 
 
-    def AttemptMove(self):
-        return true
+    def AttemptMove(self, time):
+        if self.pathIndex >= len(self.currentPath):
+            self.orderDelivered = True
+            self.timeAvailable = time + self.handoverTime
+            self.pathIndex = len(self.currentPath)
+            return False
+        elif self.pathIndex <= 0:
+            self.orderDelivered = False
+            self.busy = False
+            self.timeAvailable = time
+            return False
+
+
+        if self.pathInfo[self.pathIndex] == "A":
+            return self.Wait()
+        else:
+            return True
 
     def Wait(self):
-        number = random.randint(0, 100)
-        if number < 30:
-            return true
-        else:
-            return false
+        return random.randint(0, 100) < 30
 
 
     def Log(self, info):
